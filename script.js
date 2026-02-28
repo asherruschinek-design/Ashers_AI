d0a6f0983a945df83f1920540ae32f500553a1a3
asherruschinek-design <asherruschinek@gmail.com>
2026-02-28 20:17:14 +1100

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-const hamburger = document.getElementById('hamburger');
-const sidebar = document.getElementById('sidebar');
-
-hamburger.addEventListener('click', () => {
-  sidebar.classList.toggle('show');
-});
